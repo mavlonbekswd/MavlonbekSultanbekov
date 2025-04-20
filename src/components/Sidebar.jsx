bbaf148b938b7aa9047c41ca2aa55df019ea9e5a
mavlonbekswd <your-github-email@example.com>
2025-04-20 15:33:45 +0100

    Uptadet something

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -146,7 +146,7 @@ const Sidebar = () => {
           <ContactItem
             icon={<FaMapMarkerAlt />}
             label="LOCATION"
-            value="Cambridge U"
+            value="Cambridge, UK"
             className="text-xs md:text-[13px] lg:text-sm"
             isDark={isDark}
           />
