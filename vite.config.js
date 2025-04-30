a9533680ef862e41e086ecd7dc3cd4eb1e7b7f70
mavlonbekswd <your-github-email@example.com>
2025-04-30 22:42:53 +0100

    dafdf

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/", // <<< BU MUHIM
-  build: {
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name.endsWith('.css')) {
-            return 'styles/index.css'
-          }
-          return 'assets/[name][extname]'
-        }
-      }
-    }
-  }
+  
 })
